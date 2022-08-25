4ca97935735047fb51a5d7c59e812e7754d83fd5
Joanfa7 <jafabre95@gmail.com>
2022-08-25 18:35:40 +0000

    adding new file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,16 @@
 #!/usr/bin/env bash
-# Sets up the web server for the deployment of web_static
+# Bash script that sets up your web servers for the deplyment of web_static.
 
-apt-get update -y;
-apt-get install -y nginx;
+sudo apt-get update -y
+sudo apt-get install nginx -y
+ufw allow 'Nginx HTTP'
 
-mkdir -p /data/web_static/releases/;
-mkdir -p /data/web_static/shared/;
-mkdir -p /data/web_static/releases/test/;
-touch -p /data/web_static/releases/test/index.html;
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" >> /data/web_static/releases/test/index.html;
-
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/;
-sudo chown -R ubuntu /data/
-
-printf %s "server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-
-  root /data/web_static/current;
-  index index.html;
-
-  location /static {
-    alias /hbnb_static/;
-  }
-}
-" >> /etc/nginx/sites-available/default;
+sudo mkdir -p  /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo echo "Hello World" > /data/web_static/releases/test/index.html
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i "42i\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
 sudo service nginx restart
