08b1a1ed47230a10b59d514eb26792818af28c4f
Joanfa7 <jafabre95@gmail.com>
2022-08-25 19:49:34 +0000

    adding new file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+#create a tgz file
+import os.path
+from fabric.api import local
+from datetime import datetime
 
-from fabric.api
 
 def do_pack():
-
+    """ do_pack funtion"""
+    dt = datetime.now().strftime("%Y%m%d%H%M%S")
+    fileName = "versions/web_static_{}.tgz".format(dt)
+    if os.path.isdir("versions") is False:
+        local("mkdir version")
+    try:
+        local("tar -cvzf {} web_static".format(tgzname))
+        return tgzname
+    except:
+        return None
