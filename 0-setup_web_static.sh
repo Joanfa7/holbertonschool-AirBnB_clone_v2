decc7ccea8e11316b82e7d6dd3326d62c1d972c8
Joanfa7 <jafabre95@gmail.com>
2022-08-25 19:18:45 +0000

    adding new file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,11 +7,10 @@ sudo apt-get install nginx -y
 ufw allow 'Nginx HTTP'
 
 sudo mkdir -p  /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
 sudo mkdir -p  /data/web_static/shared
-sudo echo "Hello World" | tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo echo "Hello World" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current 
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "42i\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "40i\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
 sudo service nginx restart
