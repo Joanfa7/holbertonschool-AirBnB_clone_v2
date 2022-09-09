8c6ea9a6196c98c0c0f4b322f7cc3403baa0cbe0
Joanfa7 <jafabre95@gmail.com>
2022-09-09 15:39:27 +0000

    adding new file

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''Flask App'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    ''' return Hello HBNB'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    ''' return HBNB '''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    ''' return a '''
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/(<text>)", strict_slashes=False)
+def python(text='is cool'):
+    ''' return python is cool'''
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
