ce9e2ec9fbff6325197424ad6327c3140ba1edc2
Joanfa7 <jafabre95@gmail.com>
2022-09-12 00:14:59 +0000

    adding new file

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,10 +49,10 @@ def num_template(n):
     return render_template('5-number.html', number=n)
 
 
-@app.route('/number_odd _or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
     ''' reutrn html file '''
-    return render_template('6-number_odd_or_enven.html', number=n)
+    return render_template('6-number_odd_or_even.html', number=n)
 
 
 if __name__ == "__main__":
